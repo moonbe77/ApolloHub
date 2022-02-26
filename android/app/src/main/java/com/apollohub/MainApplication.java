ab14da3b53b6b004c9ffc442bde951d8dc43611a
Bernardo Munz <bernardo.munz@ivegroup.com.au>
2022-02-26 20:56:58 +1100

    FIX android build

--- android/app/src/main/java/com/apollohub/MainApplication.java
+++ android/app/src/main/java/com/apollohub/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import android.os.Bundle;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -47,11 +47,6 @@ public class MainApplication extends Application implements ReactApplication {
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
-
-  @Override // react navigation config
-protected void onCreate(Bundle savedInstanceState) {
-  super.onCreate(null);
-}
 
   /**
    * Loads Flipper in React Native templates. Call this in the onCreate method with something like
